d15adfd4d5a4588c662c0ab72a5d3f688de4e2df
Charles Flores <cflores@stratio.com>
2019-10-14 20:29:12 +0200

    added requirements and removed pyqt5 scrappers

--- setup.py
+++ setup.py
@@ -1,36 +1,35 @@
-import sys
-from glob import glob
+import os
 
 from setuptools import find_packages, setup
-import os
+
 pjoin = os.path.join
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-packages = []
-for d, _, _ in os.walk('scraper'):
-    if os.path.exists(pjoin(d, '__init__.py')):
-        packages.append(d.replace(os.path.sep, '.'))
 
 # Version
 ns = {}
 with open(pjoin(here, 'scraper', '_version.py')) as f:
     exec(f.read(), {}, ns)
 
+dependencies = []
+if os.path.isfile("requirements.txt"):
+    with open("requirements.txt") as f:
+        for l in f.readlines():
+            dependencies.append(l)
+
 
 def setup_package():
-
     metadata = dict(
-        name                = 'scraper',
-    	packages            = packages,
-    	description         = """Media Scraper Library.""",
-    	author              = "olbap-selrach",
-    	platforms           = "Linux",
-        version             = ns['__version__'],
-    	keywords            = ['Interactive', 'Interpreter', 'Shell', 'Web'],
-    	classifiers         = ['Programming Language :: Python :: 3'],
+        name='scraper',
+        packages=find_packages(),
+        description="""Media Scraper Library.""",
+        install_requires=dependencies,
+        author="olbap-selrach",
+        platforms="Linux",
+        version=ns['__version__'],
+        keywords=['Interactive', 'Interpreter', 'Shell', 'Web'],
+        classifiers=['Programming Language :: Python :: 3'],
     )
-
 
     setup(**metadata)
 
